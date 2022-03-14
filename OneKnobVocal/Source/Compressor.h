8421197163041a081800610861ca979609f72c35
nitin-hugar <nhugar3@gatech.edu>
2022-03-14 15:55:12 -0400

    compressor dsp

--- OneKnobVocal/Source/Compressor.h
+++ OneKnobVocal/Source/Compressor.h
@@ -18,26 +18,50 @@ public:
     Compressor() :
         apvts{ *this, nullptr, "Parameters", createParameterLayout() }
     {
-        gain.setGainDecibels(apvts.getRawParameterValue("GAIN")->load());
+        inputGain.setGainDecibels(apvts.getRawParameterValue("INPUTGAIN")->load());
+        makeUpGain.setGainDecibels(apvts.getRawParameterValue("MAKEUPGAIN")->load());
+        outputGain.setGainDecibels(apvts.getRawParameterValue("OUTPUTGAIN")->load());
+        
     }
 
     void prepareToPlay(double sampleRate, int samplesPerBlock) override
     {
         juce::dsp::ProcessSpec spec{ sampleRate, static_cast<juce::uint32> (samplesPerBlock), 2 };
-        gain.prepare(spec);
+        
+        compressor.prepare(spec);
+        inputGain.prepare(spec);
+        outputGain.prepare(spec);
+        makeUpGain.prepare(spec);
     }
 
     void processBlock(juce::AudioSampleBuffer& buffer, juce::MidiBuffer&) override
     {
-        gain.setGainDecibels(apvts.getRawParameterValue("GAIN")->load());
+        
+        
+        inputGain.setGainDecibels(apvts.getRawParameterValue("INPUTGAIN")->load());
+        outputGain.setGainDecibels(apvts.getRawParameterValue("OUTPUTGAIN")->load());
+        makeUpGain.setGainDecibels(apvts.getRawParameterValue("MAKEUPGAIN")->load());
+        
+        compressor.setAttack(apvts.getRawParameterValue("ATTACK")->load());
+        compressor.setRelease(apvts.getRawParameterValue("RELEASE")->load());
+        compressor.setThreshold(apvts.getRawParameterValue("THRESHOLD")->load());
+        compressor.setRatio(apvts.getRawParameterValue("RATIO")->load());
+        
         juce::dsp::AudioBlock<float> block(buffer);
         juce::dsp::ProcessContextReplacing<float> context(block);
-        gain.process(context);
+        
+        inputGain.process(context);
+        compressor.process(context);
+        makeUpGain.process(context);
+        outputGain.process(context);
     }
 
     void reset() override
     {
-        gain.reset();
+        inputGain.reset();
+        makeUpGain.reset();
+        outputGain.reset();
+        compressor.reset();
     }
 
     const juce::String getName() const override { return "Compressor"; }
@@ -48,8 +72,18 @@ private:
     juce::AudioProcessorValueTreeState::ParameterLayout createParameterLayout()
     {
         std::vector<std::unique_ptr<juce::RangedAudioParameter>> params;
-        params.push_back(std::make_unique<juce::AudioParameterFloat>("GAIN", "Gain", -96.0f, 12.0f, 0.0f));
+        params.push_back(std::make_unique<juce::AudioParameterFloat>("INPUTGAIN", "InputGain", -96.0f, 12.0f, 0.0f));
+        params.push_back(std::make_unique<juce::AudioParameterFloat>("OUTPUTGAIN", "OutputGain", -96.0f, 12.0f, 0.0f));
+        params.push_back(std::make_unique<juce::AudioParameterFloat>("MAKEUPGAIN", "MakeupGain", -96.0f, 12.0f, 0.0f));
+        params.push_back(std::make_unique<juce::AudioParameterFloat>("ATTACK", "Attack", 0.0f, 1000.0f, 1.0f));
+        params.push_back(std::make_unique<juce::AudioParameterFloat>("RELEASE", "Release", 0.0f, 5000.0f, 10.0f));
+        params.push_back(std::make_unique<juce::AudioParameterFloat>("THRESHOLD", "Threshold", -60.0f, 0.0f, -18.0f));
+        params.push_back(std::make_unique<juce::AudioParameterFloat>("RATIO", "Ratio", 1.0f, 100.0f, 1.0f));
+        
         return { params.begin() , params.end() };
     }
-    juce::dsp::Gain<float> gain;
+    juce::dsp::Compressor<float> compressor;
+    juce::dsp::Gain<float> inputGain;
+    juce::dsp::Gain<float> outputGain ;
+    juce::dsp::Gain<float> makeUpGain ;
 };
--- OneKnobVocal/Source/CompressorEditor.cpp
+++ OneKnobVocal/Source/CompressorEditor.cpp
@@ -13,11 +13,47 @@
 CompressorEditor::CompressorEditor(Compressor& p)
     :AudioProcessorEditor(&p), mProcessor(p)
 {
-    VolumeKnob.setSliderStyle(juce::Slider::SliderStyle::LinearHorizontal);
-    VolumeKnob.setTextBoxStyle(juce::Slider::TextBoxRight, true, 40, 20);
-    addAndMakeVisible(VolumeKnob);
-    VolumeKnobAttach = std::make_unique<juce::AudioProcessorValueTreeState::SliderAttachment>(mProcessor.apvts, "GAIN", VolumeKnob);
-    VolumeKnob.setBounds(0, 40, 120, 20);
+    InputGainKnob.setSliderStyle(juce::Slider::SliderStyle::LinearHorizontal);
+    InputGainKnob.setTextBoxStyle(juce::Slider::TextBoxRight, true, 40, 20);
+    addAndMakeVisible(InputGainKnob);
+    InputGainKnobAttach = std::make_unique<juce::AudioProcessorValueTreeState::SliderAttachment>(mProcessor.apvts, "INPUTGAIN", InputGainKnob);
+    InputGainKnob.setBounds(0, 40, 120, 20);
+    
+    ThresholdKnob.setSliderStyle(juce::Slider::SliderStyle::LinearHorizontal);
+    ThresholdKnob.setTextBoxStyle(juce::Slider::TextBoxRight, true, 40, 20);
+    addAndMakeVisible(ThresholdKnob);
+    ThresholdKnobAttach = std::make_unique<juce::AudioProcessorValueTreeState::SliderAttachment>(mProcessor.apvts, "THRESHOLD", ThresholdKnob);
+    ThresholdKnob.setBounds(0, 65, 120, 20);
+
+    RatioKnob.setSliderStyle(juce::Slider::SliderStyle::LinearHorizontal);
+    RatioKnob.setTextBoxStyle(juce::Slider::TextBoxRight, true, 40, 20);
+    addAndMakeVisible(RatioKnob);
+    RatioKnobAttach = std::make_unique<juce::AudioProcessorValueTreeState::SliderAttachment>(mProcessor.apvts, "RATIO", RatioKnob);
+    RatioKnob.setBounds(0, 90, 120, 20);
+
+    AttackKnob.setSliderStyle(juce::Slider::SliderStyle::LinearHorizontal);
+    AttackKnob.setTextBoxStyle(juce::Slider::TextBoxRight, true, 40, 20);
+    addAndMakeVisible(AttackKnob);
+    AttackKnobAttach = std::make_unique<juce::AudioProcessorValueTreeState::SliderAttachment>(mProcessor.apvts, "ATTACK", AttackKnob);
+    AttackKnob.setBounds(0, 115, 120, 20);
+
+    ReleaseKnob.setSliderStyle(juce::Slider::SliderStyle::LinearHorizontal);
+    ReleaseKnob.setTextBoxStyle(juce::Slider::TextBoxRight, true, 40, 20);
+    addAndMakeVisible(ReleaseKnob);
+    ReleaseKnobAttach = std::make_unique<juce::AudioProcessorValueTreeState::SliderAttachment>(mProcessor.apvts, "RELEASE", ReleaseKnob);
+    ReleaseKnob.setBounds(0, 140, 120, 20);
+    
+    MakeUpGainKnob.setSliderStyle(juce::Slider::SliderStyle::LinearHorizontal);
+    MakeUpGainKnob.setTextBoxStyle(juce::Slider::TextBoxRight, true, 40, 20);
+    addAndMakeVisible(MakeUpGainKnob);
+    MakeUpGainKnobAttach = std::make_unique<juce::AudioProcessorValueTreeState::SliderAttachment>(mProcessor.apvts, "MAKEUPGAIN", MakeUpGainKnob);
+    MakeUpGainKnob.setBounds(0, 165, 120, 20);
+    
+    OutputGainKnob.setSliderStyle(juce::Slider::SliderStyle::LinearHorizontal);
+    OutputGainKnob.setTextBoxStyle(juce::Slider::TextBoxRight, true, 40, 20);
+    addAndMakeVisible(OutputGainKnob);
+    OutputGainKnobAttach = std::make_unique<juce::AudioProcessorValueTreeState::SliderAttachment>(mProcessor.apvts, "OUTPUTGAIN", OutputGainKnob);
+    OutputGainKnob.setBounds(0, 190, 120, 20);
 }
 CompressorEditor::~CompressorEditor()
 {
