8a94b132636b741881847ab816f1558e010f8b3a
Sile Yin <42690744+SileYin@users.noreply.github.com>
2022-03-14 10:17:32 -0400

    Merge remote-tracking branch 'origin/alison' into develop

--- OneKnobVocal/Source/DeEsserEditor.cpp
+++ OneKnobVocal/Source/DeEsserEditor.cpp
@@ -22,31 +22,31 @@ DeEsserEditor::DeEsserEditor(OneKnobVocalAudioProcessor& p)
     ThresholdKnob.setSliderStyle(juce::Slider::SliderStyle::LinearHorizontal);
     ThresholdKnob.setTextBoxStyle(juce::Slider::TextBoxRight, true, 40, 20);
     addAndMakeVisible(ThresholdKnob);
-    ThresholdKnobAttach = std::make_unique<juce::AudioProcessorValueTreeState::SliderAttachment>(mProcessor.apvts, "THRESHOLD", ThresholdKnob);
+    ThresholdKnobAttach = std::make_unique<juce::AudioProcessorValueTreeState::SliderAttachment>(mProcessor.apvts, "DEESSER_THRESHOLD", ThresholdKnob);
     ThresholdKnob.setBounds(0, 65, 120, 20);
 
     RatioKnob.setSliderStyle(juce::Slider::SliderStyle::LinearHorizontal);
     RatioKnob.setTextBoxStyle(juce::Slider::TextBoxRight, true, 40, 20);
     addAndMakeVisible(RatioKnob);
-    RatioKnobAttach = std::make_unique<juce::AudioProcessorValueTreeState::SliderAttachment>(mProcessor.apvts, "RATIO", RatioKnob);
+    RatioKnobAttach = std::make_unique<juce::AudioProcessorValueTreeState::SliderAttachment>(mProcessor.apvts, "DEESSER_RATIO", RatioKnob);
     RatioKnob.setBounds(0, 90, 120, 20);
 
     AttackKnob.setSliderStyle(juce::Slider::SliderStyle::LinearHorizontal);
     AttackKnob.setTextBoxStyle(juce::Slider::TextBoxRight, true, 40, 20);
     addAndMakeVisible(AttackKnob);
-    AttackKnobAttach = std::make_unique<juce::AudioProcessorValueTreeState::SliderAttachment>(mProcessor.apvts, "ATTACK", AttackKnob);
+    AttackKnobAttach = std::make_unique<juce::AudioProcessorValueTreeState::SliderAttachment>(mProcessor.apvts, "DEESSER_ATTACK", AttackKnob);
     AttackKnob.setBounds(0, 115, 120, 20);
 
     ReleaseKnob.setSliderStyle(juce::Slider::SliderStyle::LinearHorizontal);
     ReleaseKnob.setTextBoxStyle(juce::Slider::TextBoxRight, true, 40, 20);
     addAndMakeVisible(ReleaseKnob);
-    ReleaseKnobAttach = std::make_unique<juce::AudioProcessorValueTreeState::SliderAttachment>(mProcessor.apvts, "RELEASE", ReleaseKnob);
+    ReleaseKnobAttach = std::make_unique<juce::AudioProcessorValueTreeState::SliderAttachment>(mProcessor.apvts, "DEESSER_RELEASE", ReleaseKnob);
     ReleaseKnob.setBounds(0, 140, 120, 20);
 
     CrossoverFreqKnob.setSliderStyle(juce::Slider::SliderStyle::LinearHorizontal);
     CrossoverFreqKnob.setTextBoxStyle(juce::Slider::TextBoxRight, true, 40, 20);
     addAndMakeVisible(CrossoverFreqKnob);
-    CrossoverFreqKnobAttach = std::make_unique<juce::AudioProcessorValueTreeState::SliderAttachment>(mProcessor.apvts, "CROSSOVERFREQ", CrossoverFreqKnob);
+    CrossoverFreqKnobAttach = std::make_unique<juce::AudioProcessorValueTreeState::SliderAttachment>(mProcessor.apvts, "DEESSER_CROSSOVERFREQ", CrossoverFreqKnob);
     CrossoverFreqKnob.setBounds(0, 165, 120, 20);
 
 }
--- OneKnobVocal/Source/GateEditor.cpp
+++ OneKnobVocal/Source/GateEditor.cpp
@@ -22,25 +22,25 @@ GateEditor::GateEditor(OneKnobVocalAudioProcessor& p)
     ThresholdKnob.setSliderStyle(juce::Slider::SliderStyle::LinearHorizontal);
     ThresholdKnob.setTextBoxStyle(juce::Slider::TextBoxRight, true, 40, 20);
     addAndMakeVisible(ThresholdKnob);
-    ThresholdKnobAttach = std::make_unique<juce::AudioProcessorValueTreeState::SliderAttachment>(mProcessor.apvts, "THRESHOLD", ThresholdKnob);
+    ThresholdKnobAttach = std::make_unique<juce::AudioProcessorValueTreeState::SliderAttachment>(mProcessor.apvts, "GATE_THRESHOLD", ThresholdKnob);
     ThresholdKnob.setBounds(0, 65, 120, 20);
 
     RatioKnob.setSliderStyle(juce::Slider::SliderStyle::LinearHorizontal);
     RatioKnob.setTextBoxStyle(juce::Slider::TextBoxRight, true, 40, 20);
     addAndMakeVisible(RatioKnob);
-    RatioKnobAttach = std::make_unique<juce::AudioProcessorValueTreeState::SliderAttachment>(mProcessor.apvts, "RATIO", RatioKnob);
+    RatioKnobAttach = std::make_unique<juce::AudioProcessorValueTreeState::SliderAttachment>(mProcessor.apvts, "GATE_RATIO", RatioKnob);
     RatioKnob.setBounds(0, 90, 120, 20);
 
     AttackKnob.setSliderStyle(juce::Slider::SliderStyle::LinearHorizontal);
     AttackKnob.setTextBoxStyle(juce::Slider::TextBoxRight, true, 40, 20);
     addAndMakeVisible(AttackKnob);
-    AttackKnobAttach = std::make_unique<juce::AudioProcessorValueTreeState::SliderAttachment>(mProcessor.apvts, "ATTACK", AttackKnob);
+    AttackKnobAttach = std::make_unique<juce::AudioProcessorValueTreeState::SliderAttachment>(mProcessor.apvts, "GATE_ATTACK", AttackKnob);
     AttackKnob.setBounds(0, 115, 120, 20);
 
     ReleaseKnob.setSliderStyle(juce::Slider::SliderStyle::LinearHorizontal);
     ReleaseKnob.setTextBoxStyle(juce::Slider::TextBoxRight, true, 40, 20);
     addAndMakeVisible(ReleaseKnob);
-    ReleaseKnobAttach = std::make_unique<juce::AudioProcessorValueTreeState::SliderAttachment>(mProcessor.apvts, "RELEASE", ReleaseKnob);
+    ReleaseKnobAttach = std::make_unique<juce::AudioProcessorValueTreeState::SliderAttachment>(mProcessor.apvts, "GATE_RELEASE", ReleaseKnob);
     ReleaseKnob.setBounds(0, 140, 120, 20);
 
 }
